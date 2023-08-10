66232451cff2e036d5706167040a2685d33da3bb
Kim Yuhan <a@bc.com>
2023-08-10 17:59:26 +0900

    8월 10일 수업 내용 업데이트

---
+++ hello-spring/src/main/java/Hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package Hello.hellospring.repository;
+
+import Hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);  // ID와 멤버를 저장
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+        // stream에서 가장 먼저 탐색되는 요소를 리턴한다.
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+}
---
+++ hello-spring/src/main/java/Hello/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package Hello.hellospring.service;
+
+import Hello.hellospring.domain.Member;
+import Hello.hellospring.repository.MemberRepository;
+import Hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository _memberRepository){
+        this.memberRepository = _memberRepository;
+    }
+
+    // 회원가입
+
+    public Long join(Member member) {
+        validateDuplicateMember (member);
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    // ifPresent() 메소드는 optional 객체가 값을 가지고 있으면 실행, 값이 없으면 넘어감
+
+    //전체 회원 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> fineOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+}
