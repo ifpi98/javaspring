77fefba48cea342eead3bd06c233a3f1fceaabaf
Kim Yuhan <a@bc.com>
2023-09-04 17:48:28 +0900

    9월 4일 수업 내용 업데이트

--- login-start/src/main/java/hello/login/web/WebConfig.java
+++ login-start/src/main/java/hello/login/web/WebConfig.java
@@ -1,16 +1,39 @@
 package hello.login.web;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
+import lombok.extern.java.Log;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new LogInterceptor())
+                    .order(1)   // 인터셉터의 호출 순서
+                    .addPathPatterns("/**") // 인터셉터 적용 URL 패턴 지정
+                    .excludePathPatterns("/css/**", "/*.ico", "/error");
+            // 인터셉터에서 제외할 패턴을 지정한다.
+
+            registry.addInterceptor(new LoginCheckInterceptor())
+                    .order(2)
+                    .addPathPatterns("/**")
+                    .excludePathPatterns(
+                            "/", "/members/add", "/login", "/logout",
+                            "/css/**", "/*.ico", "/error"
+                    );
+        }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -18,4 +41,14 @@ public class WebConfig {
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
+
+    @Bean
+    public FilterRegistrationBean LoginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
 }
--- login-start/src/main/java/hello/login/web/login/LoginController.java
+++ login-start/src/main/java/hello/login/web/login/LoginController.java
@@ -8,11 +8,13 @@ import hello.login.web.member.MemberRepository;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -84,7 +86,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request){
 
         if(bindingResult.hasErrors() ){
@@ -110,6 +112,48 @@ public class LoginController {
         // 세션에 로그인 회원 정보 보관
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
+
+        // 세션에 데이터를 보관하는 방법은 request.setAttribute(..)와  비슷하다.
+        // 하나의 세션에 여러 값을 저장할 수 있다.
+
+        // 세션의 create 옵션
+        // request.getSession(true)
+        // 세션이 있으면 기존 세션을 반환한다.
+        // 세션이 없으면 새로운 세션을 생성해서 반환한다.
+        // request.getSession(false)
+        // 세션이 있으면 기존 세션을 반환한다.
+        // 세션이 없으면 새로운 세션을 생성하지 않는다. null을 반환한다.
+
+
+
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, @RequestParam(defaultValue = "/") String redirectURL, HttpServletRequest request){
+
+        if(bindingResult.hasErrors() ){
+            log.info("errors = {}", bindingResult);
+            return "/login/loginForm";
+        }
+
+        final LoginService loginService = new LoginService(memberRepository);
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null){
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+//        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+//        response.addCookie(idCookie);
+
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        return "redirect:" + redirectURL;
 
         // 세션에 데이터를 보관하는 방법은 request.setAttribute(..)와  비슷하다.
         // 하나의 세션에 여러 값을 저장할 수 있다.
